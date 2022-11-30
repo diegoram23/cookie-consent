4bc14de8fa8c0c1a369fffac5f742516838907f3
diegoram23 <diego.pta.oc@gmail.com>
2022-11-30 11:57:06 -0800

    Submit form functioning with modal messages

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
+
+//DOM elements
 const modal = document.getElementById('modal')
+const closeBtn = document.getElementById('modal-close-btn')
+const modalText = document.getElementById('modal-text')
+const loginForm = document.getElementById('login-form')
+const modalInner = document.getElementById('modal-inner')
 
-
+//Sets modal to appear
 setTimeout(() => {
-    modal.style.display = 'block'
-}, 3000);
+    modal.style.display = 'inline'
+}, 2800);
+
+//Closes modal
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none'
+})
+
+//First modal message after users hits 'submit'
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+    <img src="loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>
+    ` 
+    //Second modal message   
+    setTimeout(() => {
+        document.getElementById('uploadText').innerText = 'Gathering your credit card information'
+    }, 2000);
+
+    //Final modal message
+    setTimeout(() => {
+       modalInner.innerHTML = `<h2>Thanks you sucker! </h2>
+        <p>You just got scammed</p>
+        <div class="idiot-gif">
+        <img src="clown.gif" class="img">
+        </div>
+        `
+    }, 4000);
+})
